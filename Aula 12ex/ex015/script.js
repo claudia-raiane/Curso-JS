a2d1effd892ecf82be6bf5913756e009d938d10c
Cláudia Raiane <101228597+claudia-raiane@users.noreply.github.com>
2023-01-15 19:01:13 -0300

    ex015 completo

--- Aula 12ex/ex015/script.js
+++ Aula 12ex/ex015/script.js
@@ -3,9 +3,47 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.querySelector('input#txtano')
     var res = document.querySelector('div#res')
-    if (fano.value.length == 0 || fano.value > ano) {
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[Erro] Tente novamente!')
     } else {
- 
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'homem'
+            if (idade > 0 && idade < 10) {
+                //Bebê
+                img.setAttribute('src', 'foto-bebe-m.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'foto-jovem-m.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'foto-adulto-m.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'foto-idoso-m.png')
+            }
+        } else {
+            genero = 'mulher'
+            if (idade > -1 && idade < 10) {
+                //Bebê
+                img.setAttribute('src', 'foto-bebe-f.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'foto-jovem-f.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'foto-adulto-f.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'foto-idoso-f.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} ano(s).`
+        res.appendChild(img)
     }
 }
