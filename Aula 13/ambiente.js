836f48a3191c0596f4ceb9d6883bdcbcd9abcbeb
Cláudia Raiane <101228597+claudia-raiane@users.noreply.github.com>
2023-01-15 22:17:24 -0300

    aula estruturas de repetição

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,33 @@
+
+/* utilizando while
+var c = 0
+while (c < 6) {
+    console.log('tudo bem?')
+    c ++
+}
+*/
+
+/* utilizando do while
+var c = 0
+do {
+    console.log('tudo bem?')
+    c ++
+} while (c < 6)
+*/
+
+/* sem estrutura de repetição
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
+
+/* utilizando for
+console.log('Vai começar...')
+for(var c = 1; c < 6; c++) {
+    console.log(c)
+}
+console.log('FIM!')
+*/
